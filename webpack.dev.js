dcd732fe249f9ec9e85ae8155c30cc0346806979
shenqi.lv <shenqi.lv@31huiyi.com>
2022-03-08 16:39:12 +0800

    增加线上展示demo

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './example/index.tsx',
@@ -19,6 +20,7 @@ module.exports = {
         hot: true,
     },
     plugins: [
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({ template: './index.html' }),
     ],
     module: {
